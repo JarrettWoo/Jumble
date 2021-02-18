f53b879a3f42042d7d341911234eb5a16291d456
JarrettWoo <jarrwoo98@gmail.com>
2021-02-18 01:44:48 +0000

    adds functionality to user input and outputs correct word

--- jumble.py
+++ jumble.py
@@ -1,3 +1,10 @@
-from english_words import english_words_lower_alpha_set as words 
+from english_words import english_words_lower_alpha_set as words
+from itertools import permutations
 
-print('ghost' in words)
+jumble_word = input("Enter jumbled word: ")
+
+possible_words = list(map("".join, permutations(jumble_word)))
+
+for i in possible_words:
+    if i in words:
+        print(i)
